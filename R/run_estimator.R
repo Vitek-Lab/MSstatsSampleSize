6e700af125134a8c875b44fe874f6cd64153a048
Ting Huang <huang.tin@husky.neu.edu>
2020-08-25 10:48:26 -0400

    update rd files

--- R/run_estimator.R
+++
@@ -1,9 +0,0 @@
-#' @export
-MSstatsSSE_shiny <- function() {
-  appDir <- system.file("sample_size_estimator", package = "MSstatsSampleSize")
-  if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `MSstatsSampleSize`.", call. = FALSE)
-  }
-  
-  shiny::runApp(appDir, display.mode = "normal")
-}
